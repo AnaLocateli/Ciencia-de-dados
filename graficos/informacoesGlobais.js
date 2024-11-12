64e53aa1d84a0b83e8c8811980e16c07ee2a4b94
AnaLocateli <107963344+AnaLocateli@users.noreply.github.com>
2024-11-12 18:54:57 +0000

    novo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,16 +1,25 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/esportes/esportes-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-
     const res = await fetch(url)
-
     const dados = await res.json()
-
     //console.log(dados)
-const paragrafo = document.createElement('p')
-paragrafo.classList.add('graficos-container__texto')
-paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-const container = document.getElementById('graficos-container')
+
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+    const pessoasPraticam = (dados.total_pessoas_que_praticam_esportes_regularmente / 1e9)
+    const pessoasAssistem = (dados.total_pessoas_que_assistem_esportes / 1e9)
+    const horas = parseInt(dados.tempo_medio_semana_praticando_esportes)
+    const minutos = Math.round((dados.tempo_medio_semana_praticando_esportes - horas ) * 60)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = 
+                `Com uma população mundial estimada em <span>${pessoasMundo}</span> bilhão de pessoas, o esporte se mostra como uma paixão global.
+                <br>Os dados revelam que <span>${pessoasPraticam}</span> bilhões de pessoas dedicam parte do seu tempo livre à prática regular de atividades físicas, representando quase metade da população mundial. Em média, essas pessoas investem ${horas}</span> horas e <span>${minutos}</span> minutos por semana em esportes.
+                <br>Além de praticar, <span>${pessoasAssistem}</span> bilhões de pessoas se declaram acompanhar jogos e competições. Esses números demonstrar o enorme potencial do setor esportivo, que movimenta bilhões de dólares e gera empregos em todo o mundo.
+                <br>O esporte é mais do que uma simples atividade de lazer. É um fenômeno global que une pessoas, culturas e economias. No entanto, apesar dos números expressivos, ainda há desafios a serem superados, como a desigualdade de acesso ao esporte em algumas regiões e a necessidade de políticas públicas que incentivem a prática esportiva.
+                `
+    const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
 
